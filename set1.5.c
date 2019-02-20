440ede22e6499931054730d9610e97cd56e58d44
PAVITHRAVINI <47105366+PAVITHRAVINI@users.noreply.github.com>
2019-02-20 16:27:57 +0530

    Create set1.5.c

---
+++ set1.5.c
@@ -0,0 +1,23 @@
+{
+int k,b,m;
+printf("enter the num: ");
+scanf("%d%d%d",&k,&b,&m);
+if(k<b)
+{
+   if(b<m)
+  {
+     printf("largest num: %d",m);
+   }
+   else 
+   if(b>k)
+    {
+      printf("largest num: %d",b);
+     }
+    }
+else
+{
+printf("largest num: %d",k);
+}
+
+    return 0;
+}
