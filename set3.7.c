cc711cc08ef8102496491dd473dd99ad50d72fd7
PAVITHRAVINI <47105366+PAVITHRAVINI@users.noreply.github.com>
2019-03-13 16:16:35 +0530

    Create set3.7.c

---
+++ set3.7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter the value");
+    scanf("%d",&a);
+    if(a==2)
+    {
+        printf("numeric");
+    }
+else
+    printf("not numeric");
+
+}
