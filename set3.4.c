0cadd9261e1e76faf625269e43264eb726adb95a
PAVITHRAVINI <47105366+PAVITHRAVINI@users.noreply.github.com>
2019-03-13 15:58:06 +0530

    Update set3.4.c

--- set3.4.c
+++ set3.4.c
@@ -2,30 +2,30 @@
 #include<stdio.h>
 int main()
 {
-	int a[10],i,j,n,temp;
+	int r[10],i,j,n,temp;
 	printf("enter the number");
 	scanf("%d",&n);
 	printf("list of element");
 	for(i=0;i<n;i++)
 	{
-	    scanf("%d",&a[i]);
+	    scanf("%d",&r[i]);
 	}
 	for(i=0;i<n-1;i++)
 	{
 	    for(j=i+1;j<n;j++)
 	    {
-	        if(a[i]>a[j])
+	        if(r[i]>r[j])
 	        {
-	        temp=a[i];
-	        a[i]=a[j];
-	        a[j]=temp;
+	        temp=r[i];
+	        r[i]=r[j];
+	        r[j]=temp;
 	        }
 	}
 	}
 	printf("sorted list \n");
 	for(i=0;i<n;i++)
 	{
-	    printf("%d",a[i]);
+	    printf("%d",r[i]);
 	}
 	return 0;
 }
