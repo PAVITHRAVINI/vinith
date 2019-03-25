6c6db6e849f5ef98d49d9749779fec813e53a0a6
PAVITHRAVINI <47105366+PAVITHRAVINI@users.noreply.github.com>
2019-03-25 15:41:10 +0530

    Create set5.1.c

---
+++ set5.1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char str[5];
+  int k,i;
+printf("enter string");
+scanf("%s",&str);
+printf("enter limit");
+scanf("%d",&k);
+for(i=0;i<=k;i++)
+{
+printf("\n%s",str);
+}
+}
