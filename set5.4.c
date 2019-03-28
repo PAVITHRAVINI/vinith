a8c02118f7ba3d31b0aa776d6e0467f3a9af1859
PAVITHRAVINI <47105366+PAVITHRAVINI@users.noreply.github.com>
2019-03-28 15:46:39 +0530

    Create set5.4.c

---
+++ set5.4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter number");
+scanf("%d",&n);
+if((n>1)&&(n<10))
+{
+printf("%d",n);
+}
+else
+{
+    printf("not in range");
+}
+}
