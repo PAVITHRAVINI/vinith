33cee7ac5fcc4dc4c8925cad27fa8d718af33d29
PAVITHRAVINI <47105366+PAVITHRAVINI@users.noreply.github.com>
2019-04-01 15:55:04 +0530

    Create set5.10.c

---
+++ set5.10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int r;
+    printf("enter number");
+    scanf("%d",&r);
+    if(r%2==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
